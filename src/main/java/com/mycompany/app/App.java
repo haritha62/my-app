2368ca2d321af9bc067af701f300fe686ce009e7
haritha62 <125486241+haritha62@users.noreply.github.com>
2023-08-07 15:57:26 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! this is Metro Devops Class" );
+        System.out.println( "Hello World! this is Metro Devops Class! we are invoking jenkins!" );
     }
 }
